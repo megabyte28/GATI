ab4ac1520d03f5802b767f076dc5e296c3d6d517
realnitinw <officialnitinw@gmail.com>
2026-03-17 13:36:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ import gym
 import gym_pybullet_drones
 from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
 from gym_pybullet_drones.utils.enums import DroneModel, Physics
+import numpy as np
 env = CtrlAviary(drone_model=DroneModel.CF2X, num_drones=1, gui=True)
 obs = env.reset()
 for i in range(1000):
-    action = env.action_space.sample() 
+    action = np.array([[15000,15000,15000,15000]]) 
     obs, reward, terminated, truncated, info = env.step(action)
     env.render()
